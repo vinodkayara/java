cc6d14ffc350437aae45d6462469b8e1db45c9b9
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2024-06-19 22:15:36 +0530

    Add files via upload

---
+++ RecursionAdvPrb/RecursionAdP1.java
@@ -0,0 +1,27 @@
+//PRINT ALL PERMUTATIONS OF STRING
+
+
+package RecursionAdvPrb;
+
+public class RecursionAdP1 {
+    public static void printPerm(String str,String permutation){
+
+        if(str.length()==0){
+        System.out.println(permutation);
+        return;
+
+    }
+
+    for(int i=0;i<str.length();i++){
+        char currChar=str.charAt(i);
+        //"abc"->"ab"
+        String newStr=str.substring(0,i)+str.substring(i+1);
+        printPerm(newStr, permutation+currChar);
+    }
+}
+public static void main(String[] args) {
+    
+    String str="abc";
+    printPerm(str,"");
+}
+}
---
+++ RecursionAdvPrb/RecursionAdP2.java
@@ -0,0 +1,26 @@
+package RecursionAdvPrb;
+
+public class RecursionAdP2 {
+    public static int countPaths(int i,int j,int n, int m){
+        if(i==n||j==m){
+            return 0;
+        }
+        if(i==n-1&&j==n-1){
+            return 1;
+        }
+        //move downwards
+        int downPaths=countPaths(i+1, j, n, m);
+
+        //move right
+        int rightPaths=countPaths(i, j+1, n, m);
+
+        return downPaths+rightPaths;
+
+    }
+    public static void main(String[] args) {
+        int n=3,m=3;
+        int totalPaths=countPaths(0, 0, n, m);
+        System.out.println(totalPaths);
+    }
+    
+}
