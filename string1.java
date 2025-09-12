9ff25f55e3c0f18c3f1db8d6ce17ea5d5bfe11e5
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2025-09-12 21:43:24 +0530

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,14 @@
+package com.stringpractice;
+
+public class string1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="vinod kayara";
+		for(int i=0;i<s.length();i++) {
+			System.out.println(s.charAt(i));
+		}
+
+	}
+
+}
